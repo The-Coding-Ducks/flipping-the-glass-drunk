47d3bca9388c86072c4492431268d2b016e7c61e
Dominic Leutenegger <leuteneggerdominic@gmail.com>
2021-09-11 19:53:24 +0200

    removed: deprecated RenderColorAndAlpha

--- code/player/DrunkenLad.cs
+++ code/player/DrunkenLad.cs
@@ -108,7 +108,7 @@ namespace FlippingTheGlassDrunk.player
 			ent.EnableHitboxes = true;
 			ent.EnableAllCollisions = true;
 			ent.SurroundingBoundsMode = SurroundingBoundsType.Physics;
-			ent.RenderColorAndAlpha = RenderColorAndAlpha;
+			ent.RenderColor = RenderColor;
 			ent.PhysicsGroup.Velocity = velocity;
 
 			if ( Local.Pawn == this )
@@ -131,7 +131,7 @@ namespace FlippingTheGlassDrunk.player
 					var clothing = new ModelEntity();
 					clothing.SetModel( model );
 					clothing.SetParent( ent, true );
-					clothing.RenderColorAndAlpha = e.RenderColorAndAlpha;
+					clothing.RenderColor = e.RenderColor;
 
 					if ( Local.Pawn == this )
 					{
